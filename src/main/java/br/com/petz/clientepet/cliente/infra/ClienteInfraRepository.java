6b91df4fc2586adbb3d34beb1c7f2a60f5193daa
Leo <nascimentoleo899@gmail.com>
2023-08-17 22:39:52 -0300

    implementa getClientes

--- src/main/java/br/com/petz/clientepet/cliente/infra/ClienteInfraRepository.java
+++ src/main/java/br/com/petz/clientepet/cliente/infra/ClienteInfraRepository.java
@@ -1,5 +1,7 @@
 package br.com.petz.clientepet.cliente.infra;
 
+import java.util.List;
+
 import org.springframework.stereotype.Repository;
 
 import br.com.petz.clientepet.cliente.application.repository.ClienteRepository;
@@ -19,6 +21,14 @@ public class ClienteInfraRepository implements ClienteRepository {
 		clienteSpringDataJPARepository.save(cliente);
 		log.info("[finaliza] ClienteInfraRepository - salva");
 		return cliente;
+	}
+
+	@Override
+	public List<Cliente> buscaTodosCliente() {
+		log.info("[inicializa] ClienteInfraRepository - buscaTodosCliente");
+		List<Cliente> clientes =  clienteSpringDataJPARepository.findAll();
+		log.info("[finaliza] ClienteInfraRepository - buscaTodosCliente");
+		return clientes;
 	}
 
 }
