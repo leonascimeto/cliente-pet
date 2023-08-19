359d742deec4b34d8abab9f53327ccf360cd494a
Leo <nascimentoleo899@gmail.com>
2023-08-19 16:24:54 -0300

    implementa pet domain

--- src/main/java/br/com/petz/clientepet/pet/domain/Pet.java
+++ src/main/java/br/com/petz/clientepet/pet/domain/Pet.java
@@ -1,5 +1,44 @@
 package br.com.petz.clientepet.pet.domain;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.validator.constraints.br.CPF;
+
+import br.com.petz.clientepet.cliente.application.api.ClienteRequest;
+import br.com.petz.clientepet.cliente.application.api.ClienteUpdateRequest;
+import jakarta.persistence.Column;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
 public class Pet {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "id", updatable = false, unique = true, nullable = false)
+	private UUID idPet;
+	@NotBlank
+	private String nome;
+	@Enumerated(EnumType.STRING)
+	private Porte porte;
+	@Enumerated(EnumType.STRING)
+	private TipoPet tipo;
+	private String microchip;
+	private String raca;
+	private SexoPet sexo;
+	private String pelagemCor;
+	private String rga;
+	private Integer peso;
+	@NotNull
+	private LocalDate dataNascimento;
+	
+	private LocalDateTime dataHoraDoCadastro;
+	private LocalDateTime dataHotaDaUltimaAlteracao;
 
 }
